f51d6359b54e001bc0f534f4a86f329c0d5780e1
felicityky <fingers160308@gmail.com>
2017-05-22 16:32:48 +0800

    Add Cat Level

--- js/app.js
+++ js/app.js
@@ -2,12 +2,20 @@
 var ViewModal = function() {
 	// Modal,也就是数据
 	this.clickCount = ko.observable(0);
+	this.level = ko.observable('NewBorn');
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
+		if(this.clickCount() >= 10) {
+			this.level('Infant');
+		}
+		if(this.clickCount() >= 30) {
+			this.level('Teen');
+		}
 	}
+
 }
 
 // apply Bindings
